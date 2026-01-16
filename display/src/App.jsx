ddebd7e4b77e9a8326af4c37904038ba521fa030
김성민 <98kimsungmin@naver.com>
2026-01-16 17:43:05 +0900

    feat: React Router 설정 추가(S14P11A301-45)

--- display/src/App.jsx
+++ display/src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
 import './App.css'
 
 function App() {
-  return <Dashboard />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        {/* 새 페이지 추가 예시: <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
