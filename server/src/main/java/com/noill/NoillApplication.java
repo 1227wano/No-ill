4de91e59a493ac07820f688c4c543ff99375ecfe
tolelom <98kimsungmin@naver.com>
2026-01-16 09:45:05 +0900

    init(server): Spring Boot 백엔드 프로젝트 초기 설정

---
+++ server/src/main/java/com/noill/NoillApplication.java
@@ -0,0 +1,13 @@
+package com.noill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoillApplication.class, args);
+    }
+
+}
